e2519b8c8153df9eea79e455ef4202176ffab4a2
Reemabdu213 <11972@holbertonstudents.com>
2026-04-07 19:47:36 +0300

    Update utils.js

--- Node_JS_basic/full_server/full_server/utils.js
+++ Node_JS_basic/full_server/full_server/utils.js
@@ -1,27 +1,22 @@
-import fs from 'fs';
+const fs = require('fs');
 
-export function readDatabase(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+const readDatabase = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+    const lines = data.trim().split('\n');
+    const students = {};
+    lines.slice(1).forEach((line) => {
+      if (line) {
+        const [firstname, , , field] = line.split(',');
+        if (!students[field]) students[field] = [];
+        students[field].push(firstname);
       }
-
-      const lines = data.trim().split('\n');
-      const students = {};
-      
-      // نتخطى السطر الأول (Header) ونعالج باقي الأسطر
-      lines.slice(1).forEach((line) => {
-        if (line) {
-          const [firstname, , , field] = line.split(',');
-          if (!students[field]) {
-            students[field] = [];
-          }
-          students[field].push(firstname);
-        }
-      });
-      resolve(students);
     });
+    resolve(students);
   });
-}
+});
+
+module.exports = readDatabase;
