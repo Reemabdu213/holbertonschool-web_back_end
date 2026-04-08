9de580a3ae4f3fbae9eb53f47116bdc53c9ee7d0
Reemabdu213 <11972@holbertonstudents.com>
2026-04-08 18:58:44 +0300

    Create utils.js

---
+++ Node_JS_basic/full_server/utils.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    const result = {};
+
+    for (const student of students) {
+      const fields = student.split(',');
+      if (fields.length < 4) continue;
+      const firstname = fields[0].trim();
+      const field = fields[3].trim();
+
+      if (!result[field]) {
+        result[field] = [];
+      }
+      result[field].push(firstname);
+    }
+
+    resolve(result);
+  });
+});
+
+export default readDatabase;
