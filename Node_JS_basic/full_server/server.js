357bdafcc290b0b8b1f88a56a5eb94fd9634a2af
Reemabdu213 <11972@holbertonstudents.com>
2026-04-08 19:21:31 +0300

    Update server.js

--- Node_JS_basic/full_server/server.js
+++ Node_JS_basic/full_server/server.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import router from './routes/index';
+const express = require('express');
+const router = require('./routes/index');
 
 const app = express();
 const port = 1245;
@@ -8,4 +8,4 @@ app.use('/', router);
 
 app.listen(port);
 
-export default app;
+module.exports = app;
