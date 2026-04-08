14413bde220db11916a52f9ada6eeeebbaabbc3a
Reemabdu213 <11972@holbertonstudents.com>
2026-04-08 19:25:52 +0300

    Update utils.js

--- Node_JS_basic/full_server/utils.js
+++ Node_JS_basic/full_server/utils.js
@@ -1,26 +1,34 @@
 const fs = require('fs');
 
-const readDatabase = (filePath) => new Promise((resolve, reject) => {
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      reject(err);
-      return;
-    }
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
-    const result = {};
-    for (const student of students) {
-      const fields = student.split(',');
-      if (fields.length < 4) continue;
-      const firstname = fields[0].trim();
-      const field = fields[3].trim();
-      if (!result[field]) {
-        result[field] = [];
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
       }
-      result[field].push(firstname);
-    }
-    resolve(result);
-  });
-});
+      const content = data.toString().split('\n');
 
-module.exports = readDatabase;
+      let students = content.filter((item) => item);
+
+      students = students.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in students) {
+        if (i !== 0) {
+          if (!fields[students[i][3]]) fields[students[i][3]] = [];
+
+          fields[students[i][3]].push(students[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+      //   return fields;
+    });
+  });
+}
+
+export default readDatabase;
